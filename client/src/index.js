063856e86dc491b52e2b486db50be898a8408342
haritejakathi <161744983+haritejakathi@users.noreply.github.com>
2024-06-29 17:18:57 +0530

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Booking from './pages/Booking';
-import AppointmentHistory from './pages/AppointmentHistory';
-import PrivateRoute from './utils/PrivateRoute';
-import { AuthProvider } from './context/AuthContext';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const App = () => {
-    return (
-        <AuthProvider>
-            <Router>
-                <Switch>
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/signin" component={SignIn} />
-                    <PrivateRoute path="/booking" component={Booking} />
-                    <PrivateRoute path="/appointments" component={AppointmentHistory} />
-                </Switch>
-            </Router>
-        </AuthProvider>
-    );
-};
-
-export default App;
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
